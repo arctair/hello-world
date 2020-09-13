2387ea4dcc9e2829953d5441f3b3fc24e05e781f
arctair <jtyler_@outlook.com>
2020-09-13 16:24:11 -0500

    test always fails

--- acceptance_test.go
+++ acceptance_test.go
@@ -4,67 +4,16 @@ package main_test
 
 import (
 	"bufio"
-	"encoding/json"
-	"fmt"
 	"io"
 	"log"
-	"net/http"
-	"os"
-	"os/exec"
-	"regexp"
 	"testing"
 	"time"
 
-	"arctair.com/hello-world/assertutil"
 	"github.com/cenkalti/backoff/v4"
 )
 
 func TestAcceptance(t *testing.T) {
-	baseUrl := os.Getenv("BASE_URL")
-	if baseUrl == "" {
-		baseUrl = "http://localhost:5000"
-
-		assertutil.NotError(t, exec.Command("sh", "build").Run())
-
-		command := exec.Command("bin/hello-world")
-		stderr, err := command.StderrPipe()
-		assertutil.NotError(t, err)
-		assertutil.NotError(t, command.Start())
-		defer dumpPipe("app:", stderr)
-		defer command.Process.Kill()
-
-		assertutil.NotError(
-			t,
-			backoff.Retry(
-				func() error {
-					_, err := http.Get(baseUrl)
-					return err
-				},
-				NewExponentialBackOff(3*time.Second),
-			),
-		)
-	}
-
-	t.Run("GET /version returns sha1 and version", func(t *testing.T) {
-		response, err := http.Get(fmt.Sprintf("%s/version", baseUrl))
-		assertutil.NotError(t, err)
-		assertutil.SuccessStatus(t, response)
-
-		var got map[string]string
-		defer response.Body.Close()
-		err = json.NewDecoder(response.Body).Decode(&got)
-		assertutil.NotError(t, err)
-
-		sha1Pattern := regexp.MustCompile("^[0-9a-f]{40}(-dirty)?$")
-		versionPattern := regexp.MustCompile("^\\d+\\.\\d+\\.\\d+$")
-
-		if !sha1Pattern.MatchString(got["sha1"]) {
-			t.Errorf("got sha1 %s want 40 hex digits", got["sha1"])
-		}
-		if !versionPattern.MatchString(got["version"]) && !sha1Pattern.MatchString(got["version"]) {
-			t.Errorf("got version %s want semver or 40 hex digits", got["version"])
-		}
-	})
+	t.Errorf("test always fails")
 }
 
 func dumpPipe(prefix string, p io.ReadCloser) {
